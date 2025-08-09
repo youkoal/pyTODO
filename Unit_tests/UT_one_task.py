61462265cb810b73df6250031239f26404cb5d68
Youkoal <ticapron@hotmail.fr>
2025-08-09 13:46:24 +0200

    fix TU

--- Unit_tests/UT_one_task.py
+++ Unit_tests/UT_one_task.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
 sys.path.append(parent_dir)
 
 from pyTODO.Models.one_task import one_task
--- Unit_tests/UT_stringUtility.py
+++ Unit_tests/UT_stringUtility.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
 sys.path.append(parent_dir)
 
 from pyTODO.utils.stringUtility import stringUtility
