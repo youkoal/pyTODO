8389404b7f32d16e8ee045e02b1c7d7db8d244b4
Youkoal <ticapron@hotmail.fr>
2025-08-09 13:57:38 +0200

    fix gallery

--- exemple_gallery/gallery.py
+++ exemple_gallery/gallery.py
@@ -1,6 +1,12 @@
+import sys
+import os
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
+
 # Copyright (C) 2022 The Qt Company Ltd.
 # SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
-from __future__ import annotations
+#from __future__ import annotations
 
 """PySide6 port of the widgets/gallery example from Qt v5.15"""
 
