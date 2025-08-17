7db40223ef3d2635620b005ee8130e5e5af35f17
Guik286 <70529517+Guik286@users.noreply.github.com>
2025-08-17 17:29:59 +0200

    interfacage 1.01

--- Unit_tests/UT_Interface.py
+++ Unit_tests/UT_Interface.py
@@ -1,7 +1,8 @@
 import sys
 import os
 from PySide6.QtWidgets import QApplication
-
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
 from View.Interface import MaFenetre
 
 
--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import sys
 import os
+#Necessaire pour lancer la QApplication pour la fenetre
+from PySide6.QtWidgets import QApplication
+
 
 parent_dir = os.path.dirname(__file__)
 sys.path.append(parent_dir)
 
 from Models.taskbox import taskbox
+#Interface
+from View.Interface import MaFenetre
 
 
 def main():
 	#self.task_boxes = [] #future implementation
-	task_box = taskbox()
-	task_box.load_json()
-	task_box.print_console()
+	#task_box = taskbox()
+	#task_box.load_json()
+	#task_box.print_console()
 
     #main_windows = ...
+	app = QApplication(sys.argv)
+	myWindow = MaFenetre()
+	myWindow.show()
+	sys.exit(app.exec())
 	
     #on passe la/les taskbox en parametre pour géré les signaux
     #main_windows.init(task_box)
