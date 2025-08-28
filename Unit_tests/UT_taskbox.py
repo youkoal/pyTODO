dcc8c1fa14b37970600a28025793e1adc35a7160
Guik286 <70529517+Guik286@users.noreply.github.com>
2025-08-28 13:42:17 +0200

    Interface 1.12

--- Unit_tests/UT_taskbox.py
+++ Unit_tests/UT_taskbox.py
@@ -3,7 +3,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from Models.one_task import one_task
 from Models.taskbox import taskbox
-import json
+
 
 
 a_task = one_task("Test Task", "This is a test task") 
@@ -18,6 +18,10 @@ a_taskbox.add_new_task(a_second_task)
 a_taskbox.add_new_task(a_third_task)
 a_taskbox.save_json()
 print(a_taskbox.get_console_extended(a_taskbox.get_tasks_todo()[0]))
+
+print(a_taskbox.get_tasks_todo()[0].get_index())
+
+
 
 
 # print("Initial Task Box with ALL checked:")
--- View/Brouillon.py
+++ View/Brouillon.py
@@ -38,3 +38,8 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
     sys.exit(app.exec())
+
+
+
+
+    
